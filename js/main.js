06a584459a7443c7917271baad3f62a2c40883e2
solocyberengineer <solocyberengineer@gmail.com>
2023-11-13 14:39:52 +0200

    added all functional requirements

--- js/main.js
+++ js/main.js
@@ -8,21 +8,29 @@ let numberBtns = document.querySelectorAll('[btn_type="number"]');
 let operatorBtns = document.querySelectorAll('[btn_type="operator"]');
 let backspace = document.querySelectorAll('[btn_type="backspace"]');
 let equalSign = document.querySelectorAll('[btn_type="result"]');
+let clearBtn = document.querySelectorAll('[btn_type="clear"]');
+let decimalpoint = document.querySelectorAll('[btn_type="decimalpoint"]');
 
 for (let i = 0; i < operatorBtns.length; i++) operators.push(operatorBtns[i].textContent);
 
-
 console.log(operatorBtns)
 
-
 for( let i = 0; i < numberBtns.length; i++ ){
-    numberBtns[i].onclick = function(){
-        if(zeroState){
-            display.textContent = numberBtns[i].textContent
-            zeroState = false
+    if(numberBtns[i].textContent != '0'){
+        numberBtns[i].onclick = function(){
+            if(zeroState){
+                display.textContent = numberBtns[i].textContent
+                zeroState = false
+            }
+            else if(!zeroState){
+                display.textContent += numberBtns[i].textContent
+            }
         }
-        else if(zeroState != true){
-            display.textContent += numberBtns[i].textContent
+    } else {
+        numberBtns[i].onclick = function(){
+            if( !zeroState ){
+                display.textContent += numberBtns[i].textContent
+            }
         }
     }
 }
@@ -40,7 +48,7 @@ backspace[0].onclick = function(){
 
 for( let i = 0; i < operatorBtns.length; i++ ){
     operatorBtns[i].onclick = function(){
-
+        zeroState = false
         let lastChar = display.textContent.at(-1);
          
         if(operatorBtns[i].textContent != lastChar){
@@ -53,7 +61,6 @@ for( let i = 0; i < operatorBtns.length; i++ ){
     }
 }
 
-
 equalSign[0].onclick = function(){
     let tempDisplay = display.textContent;
 
@@ -65,5 +72,20 @@ equalSign[0].onclick = function(){
         let divider = display.textContent.indexOf('×');
         tempDisplay = tempDisplay.replaceAll('÷', '/')
     }
+    if( display.textContent = '0' ){
+        zeroState = true;
+    }
     display.textContent = eval( tempDisplay ).toString()
+}
+
+clearBtn[0].onclick = function(){
+    display.textContent = '0'
+    zeroState = true;
+}
+
+decimalpoint[0].onclick = function(){
+    zeroState = false
+    if( display.textContent.indexOf('.') == -1){
+        display.textContent += '.'
+    }
 }
